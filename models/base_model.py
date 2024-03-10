24564b75463162de8660b51b1d0e3461118a6061
Daniel Watchman <dannietortee@gmail.com>
2024-03-10 15:53:18 +0100

    makes commitments

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Our BaseModel that defines all common attributes/methods for other classes
+"""
+import uuid
+from datetime import datetime
+import models
+
+
+class BaseModel():
+    """The main class"""
+    def __init__(self, *args, **kwargs):
+        """Constructor of the instance. Uses kwargs if not empty"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    time = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, time)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
